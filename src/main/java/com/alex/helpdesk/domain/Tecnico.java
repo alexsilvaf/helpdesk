5d28ca1c1891ef7f68d970a1e6911792f936cb32
Alex da Silva Ferreira <alex120999@hotmail.com.br>
2022-03-08 11:08:17 -0300

    Anotacoes JPA para classes de dominio

--- src/main/java/com/alex/helpdesk/domain/Tecnico.java
+++ src/main/java/com/alex/helpdesk/domain/Tecnico.java
@@ -1,18 +1,26 @@
-package com.alex.helpdesk.domain.entities;
+package com.alex.helpdesk.domain;
 
+import com.alex.helpdesk.domain.enums.Perfil;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
 public class Tecnico extends Pessoa {
 
+    @OneToMany(mappedBy = "tecnico")
     private List<Chamado> chamados = new ArrayList<>();
 
     public Tecnico() {
         super();
+        addPerfil(Perfil.TECNICO);
     }
 
     public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
         super(id, nome, cpf, email, senha);
+        addPerfil(Perfil.TECNICO);
     }
 
     public List<Chamado> getChamados() {
