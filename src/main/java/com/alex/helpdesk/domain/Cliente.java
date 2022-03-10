7a7f7583adc3ec0503be5067e9adcb5697a5480b
Alex da Silva Ferreira <alex120999@hotmail.com.br>
2022-03-10 11:38:30 -0300

    Protegendo aplicação contra serialização

--- src/main/java/com/alex/helpdesk/domain/Cliente.java
+++ src/main/java/com/alex/helpdesk/domain/Cliente.java
@@ -1,6 +1,7 @@
 package com.alex.helpdesk.domain;
 
 import com.alex.helpdesk.domain.enums.Perfil;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
@@ -10,6 +11,7 @@ import java.util.List;
 @Entity
 public class Cliente extends Pessoa {
 
+    @JsonIgnore
     @OneToMany(mappedBy = "cliente")
     private List<Chamado> chamados = new ArrayList<>();
 
