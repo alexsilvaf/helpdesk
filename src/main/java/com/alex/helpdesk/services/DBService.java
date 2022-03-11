2136782462055d8c37fc624de4f5899c531dc693
Alex da Silva Ferreira <alex120999@hotmail.com.br>
2022-03-11 01:26:22 -0300

    Endpoint findAll para técnico

--- src/main/java/com/alex/helpdesk/services/DBService.java
+++ src/main/java/com/alex/helpdesk/services/DBService.java
@@ -28,11 +28,13 @@ public class DBService {
         Tecnico tec1 = new Tecnico(null, "Valdir Cesar", "05421088014", "valdir@mail.com", "123");
         tec1.addPerfil(Perfil.ADMIN);
 
+        Tecnico tec2 = new Tecnico(null, "Alex Ferreira", "96798858015", "alex@mail.com", "123");
+
         Cliente cli1 = new Cliente(null, "Linus Torvalds", "42680014090", "torvals@mail.com", "123");
 
         Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro chamado", tec1, cli1);
 
-        tecnicoRepository.saveAll(Arrays.asList(tec1));
+        tecnicoRepository.saveAll(Arrays.asList(tec1, tec2));
         clienteRepository.saveAll(Arrays.asList(cli1));
         chamadoRepository.saveAll(Arrays.asList(c1));
     }
