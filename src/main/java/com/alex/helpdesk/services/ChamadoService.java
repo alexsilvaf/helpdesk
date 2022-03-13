3e93bcf9d5b0aca56c44b69c9ad2739cdcb30da2
Alex da Silva Ferreira <alex120999@hotmail.com.br>
2022-03-13 13:41:30 -0300

    findById para Chamado
    Signed-off-by: Alex da Silva Ferreira <alex120999@hotmail.com.br>

---
+++ src/main/java/com/alex/helpdesk/services/ChamadoService.java
@@ -0,0 +1,21 @@
+package com.alex.helpdesk.services;
+
+import com.alex.helpdesk.domain.Chamado;
+import com.alex.helpdesk.repositories.ChamadoRepository;
+import com.alex.helpdesk.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ChamadoService {
+
+    @Autowired
+    private ChamadoRepository repository;
+
+    public Chamado findById(Integer id){
+        Optional<Chamado> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: " + id));
+    }
+}
