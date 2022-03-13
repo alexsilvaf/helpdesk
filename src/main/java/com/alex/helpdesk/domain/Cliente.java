a923aadc5785606158ff1d847a2c055313c4cf3f
Alex da Silva Ferreira <alex120999@hotmail.com.br>
2022-03-13 04:17:15 -0300

    CRUD para Cliente implementado
    Signed-off-by: Alex da Silva Ferreira <alex120999@hotmail.com.br>

--- src/main/java/com/alex/helpdesk/domain/Cliente.java
+++ src/main/java/com/alex/helpdesk/domain/Cliente.java
@@ -1,5 +1,6 @@
 package com.alex.helpdesk.domain;
 
+import com.alex.helpdesk.domain.dtos.ClienteDTO;
 import com.alex.helpdesk.domain.enums.Perfil;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -7,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Entity
 public class Cliente extends Pessoa {
@@ -18,6 +20,16 @@ public class Cliente extends Pessoa {
     public Cliente() {
         super();
         addPerfil(Perfil.CLIENTE);
+    }
+
+    public Cliente(ClienteDTO obj) {
+        this.id = obj.getId();
+        this.nome = obj.getNome();
+        this.cpf = obj.getCpf();
+        this.email = obj.getEmail();
+        this.senha = obj.getSenha();
+        this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+        this.dataCriacao = obj.getDataCriacao();
     }
 
     public Cliente(Integer id, String nome, String cpf, String email, String senha) {
---
+++ src/main/java/com/alex/helpdesk/services/ClienteService.java
@@ -0,0 +1,79 @@
+package com.alex.helpdesk.services;
+
+import com.alex.helpdesk.domain.Cliente;
+import com.alex.helpdesk.domain.Pessoa;
+import com.alex.helpdesk.domain.dtos.ClienteDTO;
+import com.alex.helpdesk.repositories.ClienteRepository;
+import com.alex.helpdesk.repositories.PessoaRepository;
+import com.alex.helpdesk.services.exceptions.DataIntegrityViolationException;
+import com.alex.helpdesk.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository repository;
+
+    @Autowired
+    private PessoaRepository pessoaRepository;
+
+    public ClienteService(ClienteRepository repository, PessoaRepository pessoaRepository) {
+        this.repository = repository;
+        this.pessoaRepository = pessoaRepository;
+    }
+
+    public Cliente findById(Integer id){
+        Optional<Cliente> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id));
+    }
+
+    public List<Cliente> findAll() {
+        return repository.findAll();
+    }
+
+    public Cliente create(ClienteDTO objDTO) {
+        objDTO.setId(null);
+        validaPorCpfEEmail(objDTO);
+        Cliente newObj = new Cliente(objDTO);
+        return repository.save(newObj);
+    }
+
+    public Cliente update(Integer id, ClienteDTO objDTO) {
+        objDTO.setId(id);
+        Cliente oldObj = findById(id);
+        validaPorCpfEEmail(objDTO);
+        oldObj = new Cliente(objDTO);
+        return  repository.save(oldObj);
+    }
+
+    /**
+     * Verifica se há a existencia de uma pessoa com e-mail ou cpf inseridos
+     * @param objDTO: Objeto do tipo cliente
+     */
+    private void validaPorCpfEEmail(ClienteDTO objDTO) {
+        Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
+        if(obj.isPresent() && obj.get().getId() != objDTO.getId()){
+            throw new DataIntegrityViolationException("CPF já cadastrado no sistema!");
+        }
+
+        obj = pessoaRepository.findByEmail(objDTO.getEmail());
+        if(obj.isPresent() && obj.get().getId() != objDTO.getId()){
+            throw new DataIntegrityViolationException("Email já cadastrado no sistema!");
+        }
+    }
+
+    public void delete(Integer id) {
+        Cliente obj = findById(id);
+        if(obj.getChamados().size() > 0){
+            throw new DataIntegrityViolationException("Cliente possui ordens de serviço e não pode ser deletado!");
+        }
+        else{
+            repository.deleteById(id);
+        }
+    }
+}
